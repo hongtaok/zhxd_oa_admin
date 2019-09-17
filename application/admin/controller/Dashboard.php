7a9ebe287f9abd14a2ffdb4af131b9a00438cc8f
lgl <13653668463@163.com>
2019-09-17 13:44:47 +0800

    后台首页数据统计展示

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -3,7 +3,10 @@
 namespace app\admin\controller;
 
 use app\admin\model\User;
+use app\admin\model\Apply;
+use app\admin\model\Product;
 use app\common\controller\Backend;
+use app\common\model\Attachment;
 use think\Config;
 
 /**
@@ -36,19 +39,51 @@ class Dashboard extends Backend
         $addonVersion = isset($config['version']) ? $config['version'] : __('Unknown');
 
         $user_model = new User();
-        $totaluser = $user_model->count();
-        
+        $totaluser = $user_model->count();# 总会员数
+
+        $apply_model = new Apply();
+        $total_apply = $apply_model->count();# 总申请数
+        $total_reject_apply = $apply_model->where('status', '=', 2)->count();# 总驳回数
+        $total_apply_amount= $apply_model->where('status', '=', 3)->sum('final_check_fund');# 总放款金额
+
+        // 附件统计
+        $attachment_model = new Attachment();
+        $total_attachment = $attachment_model->count();
+
+        $product_model = new Product();
+        $total_product = $product_model->count();# 产品总数
+
+        $pcate_model = new \app\admin\model\Pcate();
+        $total_pcate = $pcate_model->count();# 产品分类数量
+
+        $article_model = new \app\admin\model\Article();
+        $total_article = $article_model->count();# 文章总数
+
+        $article_category_model = new \app\admin\model\ArticleCategory();
+        $total_article_category = $article_category_model->count();# 文章分类总数
+        $article_list = $article_model->order('id', 'desc')->limit(8)->select();
+
+        $message_model = new \app\admin\model\Message();
+        $total_message = $message_model->count();# 留言总数
+        $total_message_user = $message_model->group('user_id')->count();# 留言人数
+        $message_list = $message_model->with('user')->order('id', 'desc')->limit(8)->select();
+
         $this->view->assign([
             'totaluser'        => $totaluser,
-            'totalviews'       => 219390,
-            'totalorder'       => 32143,
-            'totalorderamount' => 174800,
-            'todayuserlogin'   => 321,
-            'todayusersignup'  => 430,
-            'todayorder'       => 2324,
-            'unsettleorder'    => 132,
-            'sevendnu'         => '80%',
-            'sevendau'         => '32%',
+            'total_apply'      => $total_apply,
+            'total_reject_apply'      => $total_reject_apply,
+            'total_apply_amount'      => $total_apply_amount,
+            'total_attachment'      => $total_attachment,
+            'total_product'      => $total_product,
+            'total_pcate'      => $total_pcate,
+            'total_article'      => $total_article,
+            'total_article_category'      => $total_article_category,
+            'total_message'      => $total_message,
+            'total_message_user'      => $total_message_user,
+
+            'article_list'      => $article_list,
+            'message_list'      => $message_list,
+
             'paylist'          => $paylist,
             'createlist'       => $createlist,
             'addonversion'       => $addonVersion,
