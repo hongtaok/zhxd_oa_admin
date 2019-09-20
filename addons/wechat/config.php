1b26384e92235ae4961583e74cf9aab05190219e
lgl <13653668463@163.com>
2019-09-20 14:48:55 +0800

    微信插件

---
+++ addons/wechat/config.php
@@ -0,0 +1,119 @@
+<?php
+
+return array (
+  0 => 
+  array (
+    'name' => 'app_id',
+    'title' => 'app_id',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'wx8204037911b1b99d',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  1 => 
+  array (
+    'name' => 'secret',
+    'title' => 'secret',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => '58cf6abcd25ac7d7275bd0c0aa50f4ae',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  2 => 
+  array (
+    'name' => 'token',
+    'title' => 'token',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'ttOQkz0Tq2XOozoY2OH8uD6YzGGtuYDD',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  3 => 
+  array (
+    'name' => 'aes_key',
+    'title' => 'aes_key',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'tS0aTS39Xljp10x3CCv9bCl99T5P9X5Rc5CXL9D0zpX',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  4 => 
+  array (
+    'name' => 'debug',
+    'title' => '调试模式',
+    'type' => 'radio',
+    'content' => 
+    array (
+      0 => '否',
+      1 => '是',
+    ),
+    'value' => '1',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  5 => 
+  array (
+    'name' => 'log_level',
+    'title' => '日志记录等级',
+    'type' => 'select',
+    'content' => 
+    array (
+      'debug' => 'debug',
+      'info' => 'info',
+      'notice' => 'notice',
+      'warning' => 'warning',
+      'error' => 'error',
+      'critical' => 'critical',
+      'alert' => 'alert',
+      'emergency' => 'emergency',
+    ),
+    'value' => 'debug',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  6 => 
+  array (
+    'name' => 'oauth_callback',
+    'title' => '登录回调',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'http://www.fangxiaopu.com/addons/wechat/index/callback',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+);
