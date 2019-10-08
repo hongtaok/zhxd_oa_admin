499e1b2482ec3b5635b75fff23cd1ae0a6173b68
lgl <13653668463@163.com>
2019-10-08 14:33:36 +0800

    阳光卡快速申请-整理

--- application/admin/controller/SuncardApply.php
+++ application/admin/controller/SuncardApply.php
@@ -11,12 +11,9 @@ use app\common\controller\Backend;
  */
 class SuncardApply extends Backend
 {
-    
-    /**
-     * SuncardApply模型对象
-     * @var \app\admin\model\SuncardApply
-     */
+
     protected $model = null;
+    protected $searchFields = ['username', 'phone'];
 
     public function _initialize()
     {
@@ -24,12 +21,7 @@ class SuncardApply extends Backend
         $this->model = new \app\admin\model\SuncardApply;
 
     }
-    
-    /**
-     * 默认生成的控制器所继承的父类中有index/add/edit/del/multi五个基础方法、destroy/restore/recyclebin三个回收站方法
-     * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
-     * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
-     */
+
     
 
 }
